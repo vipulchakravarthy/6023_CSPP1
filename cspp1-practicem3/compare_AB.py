8c679f63d4f26d329aa49d9634601e92876ed2fd
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-01 18:23:15 +0530

    sum of natural

--- cspp1-practicem3/compare_AB.py
+++ cspp1-practicem3/compare_AB.py
@@ -2,7 +2,7 @@ A = input("enter the first number")
 B = input("enter the second number")
 x = type(A)
 y = type(B)
-if (x == "str" or y == "str") :
+if (x == 'str' or y == 'str') :
 	print("strings are involved")
 elif A==B :
 	print("equal")
---
+++ cspp1-practicem3/sumofnatural.py
@@ -0,0 +1,5 @@
+sum = 0
+n = int(input("enter the number"))
+for n in range(1,n+1):
+    sum=sum+n
+print(sum)
