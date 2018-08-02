05362a6e5ce08c4ea9b3b6fa09c0f2da7675c3ef
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 18:54:31 +0530

    " -> 0 of 5 passed. pylint: -2.00/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -5,15 +5,13 @@
 def main():
   '''
   #the program is to check how many times
-  #bob is printed in 
-  #given 
-  #string
+  #bob is printed in given string
   '''
-main_string = str(input())
-length_string = len(main_string)
-count_var = 0
-for ind in range(length_string-2):
-    if main_string[ind] == "b" and main_string[ind+1] == "o" and main_string[ind+2] == "b":
+  m_sent = str(input())
+  length_m = len(m_sent)
+  count_var = 0
+  for ind in range(length_m-2):
+    if m_sent[ind] == "b" and m_sent[ind+1] == "o" and m_sent[ind+2] == "b":
         count_var += 1
 print(count_var)
 if __name__ == "__main__":
