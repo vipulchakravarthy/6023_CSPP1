200504a22acdb3cb1656396bf131fb8aa0b43edf
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 19:37:44 +0530

    " -> 5 of 5 passed. pylint: 8.75/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -4,9 +4,9 @@
 #alphabetical order.In the case of ties, print the first substring.
 '''
 def main():
-     ''' 
-     #the program is to print longest substring in the given string
-     '''
+    ''' 
+         #the program is to print longest substring in the given string
+    '''
     imp_sentence = str(input())
     imp_sent = str.lower(imp_sentence)
     maxim_substr = []
@@ -15,10 +15,10 @@ def main():
         count_var = 0
         while i + 1 < len(imp_sent) and imp_sent[i] <= imp_sent[i+1]:
             count_var += 1
-             i += 1
-             sub_string += imp_sent[i]
-     if len(sub_string) > len(maxim_substr):
-        maxim_substr = sub_string
+            i += 1
+            sub_string += imp_sent[i]
+        if len(sub_string) > len(maxim_substr):
+            maxim_substr = sub_string
     print(maxim_substr)
     # the input string is in s
     # remove pass and start your code here
