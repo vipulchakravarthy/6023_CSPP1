0539e60912a04b697d42e4f5d6b23a8ccff90ade
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 18:57:38 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -3,16 +3,16 @@
 #prints the number of times the string 'bob' occurs in s.
 '''
 def main():
-  '''
+    '''
   #the program is to check how many times
   #bob is printed in given string
   '''
-  m_sent = str(input())
-  length_m = len(m_sent)
-  count_var = 0
-  for ind in range(length_m-2):
-    if m_sent[ind] == "b" and m_sent[ind+1] == "o" and m_sent[ind+2] == "b":
-        count_var += 1
-  print(count_var)
+    m_sent = str(input())
+    length_m = len(m_sent)
+    count_var = 0
+    for ind in range(length_m-2):
+        if m_sent[ind] == "b" and m_sent[ind+1] == "o" and m_sent[ind+2] == "b":
+            count_var += 1
+    print(count_var)
 if __name__ == "__main__":
     main()
