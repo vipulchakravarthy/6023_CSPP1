9d3520c0a1e2b333537fc41f71ff869dff423303
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 17:53:33 +0530

    " -> 0 of 4 passed. pylint: -6.00/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -8,7 +8,7 @@
 def main():
     '''
     #Write a program that counts up the number of vowels contained in the string s.
-    #Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl',    
+    #Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
     '''
     s = raw_input()
     # the input string is in s
