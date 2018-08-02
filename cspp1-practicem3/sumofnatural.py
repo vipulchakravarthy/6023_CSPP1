cb5e831954e80906478f19d632efeebe7ee8cfc6
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 12:28:50 +0530

    " -> 0 of 1 passed. pylint: 6.00/10 "

--- cspp1-practicem3/sumofnatural.py
+++ cspp1-practicem3/sumofnatural.py
@@ -1,5 +1,7 @@
-sum = 0
-n = int(input("enter the number"))
-for n in range(1,n+1):
-    sum=sum+n
-print(sum)
+""" this is a program to print sum of n natural numbers """
+SUM_NATURAL = 0
+NUMBER_N = int(input("enter the number"))
+for NUMBER_N in range(1, NUMBER_N+1):
+    SUM_NATURAL = SUM_NATURAL + NUMBER_N
+
+print(SUM_NATURAL)
