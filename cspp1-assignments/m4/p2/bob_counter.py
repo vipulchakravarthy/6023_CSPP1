cf0fc0ca048760c6578fac523f646e89fae9340b
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 18:29:29 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -3,14 +3,13 @@ Write a program that prints the number of times the string 'bob' occurs in s.
 For example, if s = 'azcbobobegghakl', then your program should print
 Number of times bob occurs is: 2'''
 def main():
- """ # the input string is in s
- # remove pass and start your code here """
-m_s = str(input())
-length_int = len(m_s)
-count = 0
-for ind in range(length_int-2):
-        if m_s[ind] == "b" and m_s[ind+1] == "o" and m_s[ind+2] == "b":
-            count += 1
-print( count)
-if __name__== "__main__":
+"""the input string is in s remove pass and start your code here"""
+main_string = str(input())
+length_string = len(m_s)
+count_var = 0
+for ind in range(length_string-2) :
+  if main_string[ind] == "b" and main_string[ind+1] == "o" and main_string[ind+2] == "b":
+    count_var += 1
+print( count_var)
+if __name__ == "__main__":
     main()
