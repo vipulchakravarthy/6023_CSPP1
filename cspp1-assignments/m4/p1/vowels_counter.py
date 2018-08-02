386ac102e2ecf9d054728fc1e6d4f3ab16b2485c
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-02 17:50:14 +0530

    " -> 0 of 4 passed. pylint: -10.00/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -1,7 +1,7 @@
 #Assume s is a string of lower case characters.
 
 #Write a program that counts up the number of vowels contained in the string s.
-#Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', 
+#Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl',
 #your program should print:
 
 #Number of vowels: 5
@@ -12,5 +12,6 @@ def main():
     # remove pass and start your code here
     pass
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
+    
