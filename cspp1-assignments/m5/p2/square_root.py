429372cd3f9db6683be353a0ec1248d144922784
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-03 18:45:11 +0530

    " -> 3 of 3 passed. pylint: 9.17/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -1,14 +1,10 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
+''' Write a python program to find the square root of the
+given number using approximation method.
+'''
 def main():
+    ''' the problem given is to find the square root of the
+    given number using approximation
+    '''
     number = int(input())
     epsilon = 0.01
     step = 0.1
@@ -17,8 +13,7 @@ def main():
         if abs(check**2 - number) >= epsilon:
             check += step
         else:
-             break
+            break
     print(check)
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
