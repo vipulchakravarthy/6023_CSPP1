818a0814baab6833063005028c3cd4008f7b8a1b
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-03 17:12:31 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/m5/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -2,20 +2,20 @@
 given number using bisection method
 '''
 def main():
-    ''' the problem is to find square root of a number using
+   ''' the problem is to find square root of a number using
     bisection method
-    ''' 
+   '''
     epsilon = 0.01
     number = int(input())
     low = 0.0
     high = number
     output = (low + high)/2.0
     while abs(output**2 - number) >= epsilon:
-        if output**2 < number :
+        if output**2 < number:
             low = output
         else:
             high = output
         output = (low + high)/2.0
     print(output)
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
