24abf1838a850684777cdb0f4054c134b0319096
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-03 15:28:37 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

---
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -0,0 +1,22 @@
+''' Write a python program to find if the given number is a
+perfect cube or not using guess and check algorithm
+'''
+def main():
+	''' the problem is about to check whether the given
+	 number is perfect cube or not
+	 '''
+	cube_number = int(input())
+    epsilon = 0.01
+    step = 1
+    guess = 0
+    while guess <= cube_number:
+        if abs(guess**3 -cube_number) >= epsilon:
+            guess += step
+        else:
+            break
+    if abs(guess**3 - cube_number) >= epsilon:
+        print(cube_number + 'not a perfect cube')
+    else:
+        print(cube_number + 'is  a perfect cube ')
+if __name__== "__main__":
+	main()
