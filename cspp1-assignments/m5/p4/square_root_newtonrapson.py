f558113bcf52e986ef3ca0f02b9e8aa9fda95280
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-03 18:24:04 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p4/square_root_newtonrapson.py
@@ -2,12 +2,12 @@
 number using newton-raphson method
 '''
 def main():
-    ''' the problem is to find square root of a given number 
+    ''' the problem is to find square root of a given number
     using newton-raphson method.
     '''
     number = int(input())
     epsilon = 0.01
-    check = ( number / 2.0)
+    check = (number / 2.0)
     while abs(check**2 - number) >= epsilon:
         check = check - ((check**2 - number) / (2*check))
     print(check)
