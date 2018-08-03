ed82fba7d805bbdbf9851c8ed6a946417bc5cd9e
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-03 15:47:55 +0530

    " -> 0 of 3 passed. pylint: 7.33/10 "

---
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -0,0 +1,22 @@
+'''# Write a python program to find the square root of the
+given number using bisection method
+'''
+def main():
+    ''' the problem is to find square root of a number using
+    bisection method
+    ''' 
+    epsilon = 0.01
+    number = int(input())
+    epsilon = 0.01
+    low = 1.0
+    high = number
+    output = (low + high)/2.0
+    while abs(output**2 - number) >= epsilon:
+        if output**2 < number :
+            low = output
+        else:
+            high = output
+        output = (low + high)/2.0
+    print(output)
+if __name__== "__main__":
+    main()
