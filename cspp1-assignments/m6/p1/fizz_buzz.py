3bc6c61788733a3ebc97e6c6734fd7a974d448f7
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-04 10:29:35 +0530

    " -> 7 of 7 passed. pylint: 7.69/10 "

--- cspp1-assignments/m6/p1/fizz_buzz.py
+++ cspp1-assignments/m6/p1/fizz_buzz.py
@@ -2,23 +2,25 @@
 Write a short program that prints each number from 1 to num on a new line.
 For each multiple of 3, print "Fizz" instead of the number.
 For each multiple of 5, print "Buzz" instead of the number.
-For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of
+the number.
 '''
 def main():
     '''
-    Read number from the input, store it in variable num.
+    program is to print 1 to n number and if number is divisible by 3 print Fizz
+    and if it is divisible by 5 print Buzz and if it is done by both print Fizz Buzz.
     '''
     number = int(input())
-
-    for n in range(1,number+1):
-            if n%3== 0 and n%5 == 0:
-               print("Fizz")
-               print("Buzz")
-            elif n%3 == 0:
-                print("Fizz")
-            elif n%5 == 0:
-                print("Buzz")
-            else :
-               print(n)
+    for num_n in range(1, number+1):
+        if num_n%3== 0 and num_n%5 == 0:
+            print("Fizz")
+            print("Buzz")
+        elif num_n%3 == 0:
+            print("Fizz")
+        elif num_n%5 == 0:
+            print("Buzz")
+        else:
+            print(num_n)
 if __name__  == "__main__":
     main()
+
