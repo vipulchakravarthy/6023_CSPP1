834c4592328ab4a2d9f24c626258dcd04bbbe455
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-07 15:05:21 +0530

    gcd_recursion

--- cspp1-assignments/m8/GCD using Recursion/gcd_recr.py
+++ cspp1-assignments/m8/GCD using Recursion/gcd_recr.py
@@ -1,9 +1,8 @@
-# Exercise: GCDRecr
-# Write a Python function, gcdRecur(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.
-
-# This function takes in two numbers and returns one number.
-
-
+''' Exercise: GCDRecr
+ Write a Python function, gcdRecur(a, b), that takes in two
+ numbers and returns the GCD(a,b) of given a and b.
+ This function takes in two numbers and returns one number.
+'''
 def gcdRecur(a, b):
     '''
     a, b: positive integers
@@ -11,13 +10,15 @@ def gcdRecur(a, b):
     returns: a positive integer, the greatest common divisor of a & b.
     '''
     if b == 0 :
-    	return a
-    else
-        return gcd(b, a%b)
+        return a
+    else:
+        return gcdRecur(b, a%b)
 def main():
+    ''' the program is to perform the gcd of two numbers of using 
+    recursion
+    '''
     data = input()
     data = data.split()
     print(gcdRecur(int(data[0]),int(data[1])))
-
 if __name__== "__main__":
     main()
