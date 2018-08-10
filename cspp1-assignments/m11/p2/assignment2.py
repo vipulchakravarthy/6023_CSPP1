ab29e49bd39d06b26d5e8cdf87efef5d55d25b14
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-10 18:59:15 +0530

    assignment-2

---
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -0,0 +1,38 @@
+#Exercise: Assignment-2
+#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+import copy
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    temp = hand.copy()
+    for i in word: 
+        #if  i in temp:
+            temp[i] -= 1         
+    return temp
+def main():
+	n = input()
+	adict = {}
+	for i in range(int(n)):
+		data = input()
+		l = data.split()
+		adict[l[0]] = int(l[1])
+	data1 = input()
+	print(updateHand(adict,data1))
+		
+
+
+if __name__== "__main__":
+	main()
