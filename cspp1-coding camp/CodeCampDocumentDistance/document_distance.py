258139374438be69d711774c8dded5643f579701
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-17 14:16:18 +0530

    " -> 2 of 3 passed. pylint: 9.14/10 "

--- cspp1-coding camp/CodeCampDocumentDistance/document_distance.py
+++ cspp1-coding camp/CodeCampDocumentDistance/document_distance.py
@@ -2,7 +2,7 @@
     Document Distance - A detailed description is given in the PDF
 '''
 import math
-filename = "stopwords.txt"
+file_name = "stopwords.txt"
 
 def similarity(dict1, dict2):
     '''
@@ -12,7 +12,7 @@ def similarity(dict1, dict2):
     temp2 = {}
     sum1 = 0
     sum_d1 = 0
-    sum_d2 =0
+    sum_d2 = 0
     for word in dict1:
         if word in temp1:
             temp1[word] += 1
@@ -26,12 +26,11 @@ def similarity(dict1, dict2):
     word_freq = {}
     keys = set(list(temp1.keys()) + list(temp2.keys()))
     for key in keys:
-      word_freq[key] =  [0, 0]
+        word_freq[key] =  [0, 0]
     for key in temp1:
         word_freq[key][0] = temp1[key]
     for key in temp2:
-            word_freq[key][1] = temp2[key]
-    print(word_freq)
+        word_freq[key][1] = temp2[key]
     for key in word_freq:
         sum1 = sum1 + (word_freq[key][0] * word_freq[key][1])
     numerator = sum1
@@ -40,15 +39,14 @@ def similarity(dict1, dict2):
         sum_d2 = sum_d2 + (word_freq[key][1]**2)
     denominator = (math.sqrt(sum_d1) * math.sqrt(sum_d2))
     similarity = numerator/ denominator
-    print("similarity is :")
     return round(similarity, 1)
-def load_stopwords(filename):
+def load_stopwords(file_name):
     '''
         loads stop words from a file and returns a dictionary
     '''
     stopwords = {}
-    with open(filename, 'r') as filename:
-        for line in filename:
+    with open(file_name, 'r') as file_name:
+        for line in file_name:
             stopwords[line.strip()] = 0
     return stopwords
 
@@ -60,16 +58,15 @@ def main():
     input2 = str.lower(input())
     character = '.,!?$%^&*()1234567890'
     for char in character:
-        input1 = input1.replace(char, '' )
+        input1 = input1.replace(char, '')
         input2 = input2.replace(char, '')
     input_list1 = input1.split()
     input_list2 = input2.split()
+    stop_words = load_stopwords(file_name)
     for word in input_list1:
-        stop_words = load_stopwords(filename)
         if word in stop_words.keys():
             input_list1.remove(word)
     for word in input_list2:
-        stopwords = load_stopwords(filename)
         if word in stop_words.keys():
             input_list2.remove(word)
     print(similarity(input_list1, input_list2))
