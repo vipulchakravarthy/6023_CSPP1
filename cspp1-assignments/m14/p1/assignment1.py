f24f5e7a825b6adbbc7c4834648525032ef1c26e
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-21 20:44:13 +0530

    " -> 4 of 5 passed. pylint: 9.07/10 "

--- cspp1-assignments/m14/p1/assignment1.py
+++ cspp1-assignments/m14/p1/assignment1.py
@@ -77,7 +77,6 @@ class Message(object):
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-        
         Returns: self.message_text
         '''
         return self.message_text
@@ -85,24 +84,20 @@ class Message(object):
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class
-        
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
-        
     def build_shift_dict(self, shift):
         '''
         Creates a dictionary that can be used to apply a cipher to a letter.
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
-        
-        shift (integer): the amount by which to shift every letter of the 
+        letters only.
+        shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
-
         Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
+        another letter (string).
         '''
         cap = lambda char, shift: chr((ord(char)-65 + shift)%26 +65)
         small = lambda char, shift: chr((ord(char)-97 + shift)%26 +97)
@@ -122,13 +117,11 @@ class Message(object):
         '''
         Applies the Caesar Cipher to self.message_text with the input shift.
         Creates a new string that is self.message_text shifted down the
-        alphabet by some number of characters determined by the input shift        
-        
+        alphabet by some number of characters determined by the input shift
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
-
         Returns: the message text (string) in which every character is shifted
-             down the alphabet by the input shift
+        down the alphabet by the input shift
         '''
         new_text = []
         for letter in self.message_text:
