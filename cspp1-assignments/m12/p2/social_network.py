707b30c38fb99f1acb4a870aee1524fa77e4d6f3
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-23 07:15:28 +0530

    " -> 1 of 2 passed. pylint: 9.70/10 "

--- cspp1-assignments/m12/p2/social_network.py
+++ cspp1-assignments/m12/p2/social_network.py
@@ -13,7 +13,7 @@ def follow(network, arg1, arg2):
         so, this should result in adding arg2 to the followers list of arg1
         update the network dictionary and return it
     '''
-    if arg1 in network.keys():
+    if arg1 in network.keys() and arg2 in network:
         network[arg1].append(arg2)
     else:
         network[arg1] = arg2
