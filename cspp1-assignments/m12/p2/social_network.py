5f15f792589a0c1cb729fa5dbf95ebee9240d2f6
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-23 07:16:15 +0530

    " -> 2 of 2 passed. pylint: 9.70/10 "

--- cspp1-assignments/m12/p2/social_network.py
+++ cspp1-assignments/m12/p2/social_network.py
@@ -16,7 +16,7 @@ def follow(network, arg1, arg2):
     if arg1 in network.keys() and arg2 in network:
         network[arg1].append(arg2)
     else:
-        network[arg1] = arg2
+        network[arg1] = [arg2]
     return network
 def unfollow(network, arg1, arg2):
     '''
