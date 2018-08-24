a8a7727ad9c8ce2ae03b748a50543b44d330d37c
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-24 10:22:02 +0530

    " -> 13 of 14 passed. pylint: -6.29/10 "

--- cspp1-code camp/tictactoe/tic_tac_toe.py
+++ cspp1-code camp/tictactoe/tic_tac_toe.py
@@ -10,8 +10,10 @@ def is_winner(tic_list):
 		for j in range(3):
 			if tic_list[i][j] != 'x' and tic_list[i][j] != 'o' and tic_list[i][j] != '.':
 				flag = 1
-	if flag == 1 or count_dict['x'] >= 5 or count_dict['o'] >= 5:
+	if flag == 1:
 		return "invalid input"
+	if count_dict['x'] >= 5 or count_dict['o'] >= 5:
+		return "invalid game"
 	if (tic_list[0][0] == tic_list[0][1] == tic_list[0][2] == 'x' or
 		tic_list[1][0] == tic_list[1][1] == tic_list[1][2] == 'x' or
 		tic_list[2][0] == tic_list[2][1] == tic_list[2][2] == 'x' or
