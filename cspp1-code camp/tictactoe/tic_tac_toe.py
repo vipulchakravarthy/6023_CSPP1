ed032dab664af4466ee7a2eda7c70608853d6383
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-24 10:37:44 +0530

    " -> 14 of 14 passed. pylint: 4.57/10 "

--- cspp1-code camp/tictactoe/tic_tac_toe.py
+++ cspp1-code camp/tictactoe/tic_tac_toe.py
@@ -1,4 +1,4 @@
-from collections import Counter 
+from collections import Counter
 def is_winner(tic_list):
     '''
     takes a list as input and returns who is the winner
@@ -11,30 +11,31 @@ def is_winner(tic_list):
             count_dict[char] += 1
     for row in range(3):
         for column in range(3):
-            if tic_list[row][column] != 'x' and tic_list[row][column] != 'o' and tic_list[row][column] != '.':
+            if (tic_list[row][column] != 'x' and tic_list[row][column] != 'o'
+                and tic_list[row][column] != '.'):
                 flag = 1
     if flag == 1:
         return "invalid input"
     if count_dict['x'] > 5 or count_dict['o'] > 5:
         return "invalid game"
     if(tic_list[0][0] == tic_list[0][1] == tic_list[0][2] == 'x' or
-            tic_list[1][0] == tic_list[1][1] == tic_list[1][2] == 'x' or
-            tic_list[2][0] == tic_list[2][1] == tic_list[2][2] == 'x' or
-            tic_list[0][0] == tic_list[1][1] == tic_list[2][2] == 'x' or
-            tic_list[0][0] == tic_list[1][0] == tic_list[2][0] == 'x' or
-            tic_list[0][2] == tic_list[1][1] == tic_list[2][0] == 'x' or
-            tic_list[0][2] == tic_list[1][2] == tic_list[2][2] == 'x' or
-            tic_list[0][1] == tic_list[1][1] == tic_list[2][1] == 'x' ):
+        tic_list[1][0] == tic_list[1][1] == tic_list[1][2] == 'x' or
+        tic_list[2][0] == tic_list[2][1] == tic_list[2][2] == 'x' or
+        tic_list[0][0] == tic_list[1][1] == tic_list[2][2] == 'x' or
+        tic_list[0][0] == tic_list[1][0] == tic_list[2][0] == 'x' or
+        tic_list[0][2] == tic_list[1][1] == tic_list[2][0] == 'x' or
+        tic_list[0][2] == tic_list[1][2] == tic_list[2][2] == 'x' or
+        tic_list[0][1] == tic_list[1][1] == tic_list[2][1] == 'x'):
         count += 1
-        winner =  'x'
+        winner = 'x'
     if(tic_list[0][0] == tic_list[0][1] == tic_list[0][2] == 'o' or
-            tic_list[1][0] == tic_list[1][1] == tic_list[1][2] == 'o' or
-            tic_list[2][0] == tic_list[2][1] == tic_list[2][2] == 'o' or
-            tic_list[0][0] == tic_list[1][1] == tic_list[2][2] == 'o' or
-            tic_list[0][0] == tic_list[1][0] == tic_list[2][0] == 'o' or
-            tic_list[0][2] == tic_list[1][1] == tic_list[2][0] == 'o' or
-            tic_list[0][2] == tic_list[1][2] == tic_list[2][2] == 'o' or
-            tic_list[0][1] == tic_list[1][1] == tic_list[2][1] == 'o' ):
+        tic_list[1][0] == tic_list[1][1] == tic_list[1][2] == 'o' or
+        tic_list[2][0] == tic_list[2][1] == tic_list[2][2] == 'o' or
+        tic_list[0][0] == tic_list[1][1] == tic_list[2][2] == 'o' or
+        tic_list[0][0] == tic_list[1][0] == tic_list[2][0] == 'o' or
+        tic_list[0][2] == tic_list[1][1] == tic_list[2][0] == 'o' or
+        tic_list[0][2] == tic_list[1][2] == tic_list[2][2] == 'o' or
+        tic_list[0][1] == tic_list[1][1] == tic_list[2][1] == 'o'):
         count += 1
         winner = 'o'
     if count == 2:
@@ -48,11 +49,10 @@ def main():
     the program is to decide the winner of tic tac toe game
     '''
     tic_list = []
-    for iterate in range(3):
-        row = list(map(str,input().split()))
+    for _ in range(3):
+        row = list(map(str, input().split()))
         tic_list.append(row)
     print(is_winner(tic_list))
 if __name__ == '__main__':
     main()
-
 
