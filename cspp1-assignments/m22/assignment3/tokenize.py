4b44622acb161e914f6bf444a915b5dda48bd6d4
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-25 10:58:03 +0530

    " -> 3 of 4 passed. pylint: 9.29/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -3,21 +3,21 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 def tokenize(word_list):
-	''' takes the list of words and gives the dictionary which contains frequency of words
-	'''
+    ''' takes the list of words and gives the dictionary which contains frequency of words
+    '''
     dictionary = {}
     for iterate in word_list:
-    	if iterate in dictionary:
-    		dictionary[iterate] += 1
-    	else:
-    		dictionary[iterate] = 1
+        if iterate in dictionary:
+            dictionary[iterate] += 1
+        else:
+            dictionary[iterate] = 1
     return dictionary       
 def main():
-	''' to take the input and give the frequency of words in that string
-	'''
-	lines = int(input())
-	for _ in range(lines):
-		word_list = input().split()
-	print(tokenize(word_list))
+    ''' to take the input and give the frequency of words in that string
+    '''
+    lines = int(input())
+    for _ in range(lines):
+        word_list = input().split()
+    print(tokenize(word_list))
 if __name__ == '__main__':
     main()
