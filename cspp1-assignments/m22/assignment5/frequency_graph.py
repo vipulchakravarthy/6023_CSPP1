4ee5c03765e49df26d914c657a99ed7d7a8d4c60
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-25 12:00:01 +0530

    " -> 4 of 4 passed. pylint: 9.00/10 "

--- cspp1-assignments/m22/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/frequency_graph.py
@@ -7,12 +7,12 @@ def frequency_graph(dictionary):
     shoud be in form of #
     '''
     for key in sorted(dictionary):
-        i = dictionary[key]
-        character = "#" * i 
+        value = dictionary[key]
+        character = "#" * value
         print(key, "-", character)
 def main():
     '''the program is to print the keys and values of a dicitonary
-    ''' 
+    '''
     dictionary = eval(input())
     frequency_graph(dictionary)
 if __name__ == '__main__':
